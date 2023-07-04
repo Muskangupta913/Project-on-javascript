fa69fdade8353217d68c1528fde9a9570ed35160
Muskangupta913 <102994661+Muskangupta913@users.noreply.github.com>
2023-07-05 00:09:29 +0530

    Add files via upload

---
+++ countdown timer/js/app.js
@@ -0,0 +1,29 @@
+const endDate = "20 March 2022 10:00PM";
+document.getElementById("end-date").innerText=endDate;
+const input=document.querySelectorAll("input")
+
+
+
+
+
+
+
+function clock(){
+    const end = new Date(endDate);
+    const now =new Date();
+    const diff =end-now;
+    //convert into days;
+    input[0].value=Math.floor(diff/3600/24);
+    input[1].value=Math.floor(diff / 3600 )%24;
+    input[2].value=Math.floor(diff / 60 )%60;
+    input[1].value=Math.floor(diff) %60;
+
+}
+clock()
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+
+)
